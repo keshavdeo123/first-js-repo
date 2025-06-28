f6706da99f8adc566008d94294405a30f16d758b
Keshav Deo <keshavmaharaj345@gmail.com>
2025-06-28 12:49:42 +0000

    day2

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log("5">1);
+// console.log(3<5);
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+//console.log("2"===2);    Falses because it check data type and values as well
+
+
+
+
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,4 @@
+// Primitive 7 Types => String , Number, Boolean, null,Undefined , symbol , BigInt
+
+// Refrence (Non Primitive )
+// Arrays , Objects , Functions
