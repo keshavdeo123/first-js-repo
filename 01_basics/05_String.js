ab109a0e3229ef8e2bc8a36c0f1157c6c8633e77
Keshav Deo <keshavmaharaj345@gmail.com>
2025-06-29 05:30:11 +0000

    string

---
+++ 01_basics/05_String.js
@@ -0,0 +1,12 @@
+const Name="Keshav"
+const age=20
+
+//console.log(Name+age+" Engineer");  Very Outdated don't use
+
+console.log(`My name is ${Name} And i'm ${age} years old`);
+
+const gameName = new String('Keshav');
+console.log(gameName);
+console.log(gameName[1]);
+console.log(gameName.length);
+console.log(gameName.__proto__);
