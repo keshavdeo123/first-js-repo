8a2c15a9d784391cffc4c3a1c42edf7d4f552626
Keshav Deo <keshavmaharaj345@gmail.com>
2025-06-29 04:50:27 +0000

    dataTypes done

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,3 +2,28 @@
 
 // Refrence (Non Primitive )
 // Arrays , Objects , Functions
+
+//******************************************************************************************************************** */
+// Stack And Heap Memory
+// stack(Primitive ) , Heap(NOn Primitive)
+
+let myEmail="Keshav@1223"
+
+let NewEmail=myEmail
+NewEmail="deo123"
+
+console.log(NewEmail);
+console.log(myEmail);
+
+let userOne = {
+    Name:"Keshav Deo",
+    email:"keshav@123"
+}
+let userTwo=userOne;
+
+userOne.email="Deo@123"
+
+console.log(userOne.email);
+console.log(userTwo.email)
+
+
