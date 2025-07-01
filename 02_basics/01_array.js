bdf513e672f3a015028c38f586ded693701b53a8
Keshav Deo <keshavmaharaj345@gmail.com>
2025-07-01 11:29:05 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+const array1=[1,4,3,67,8]
+const array2=["Keshav"+"Deo"]
+
+const array3=new Array(1,3,45,67,8)
+// console.log(array3);
+
+array1.push(110)
+array1.pop()
+console.log(array1);
+console.log(array1.includes(19));
+console.log(array1.indexOf(19));
+console.log(array1.indexOf(67));
+
+const newArray=array1.join()
