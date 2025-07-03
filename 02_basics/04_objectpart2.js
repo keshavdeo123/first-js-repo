9acd6295c99affc072bc6365d5db35ec9d7dac0c
Keshav Deo <keshavmaharaj345@gmail.com>
2025-07-03 16:49:05 +0000

    prt2

---
+++ 02_basics/04_objectpart2.js
@@ -0,0 +1,16 @@
+//const Tinder=new Object() // Singleton object 
+
+const Tinder={} // non singleton object
+// Dono cases me output same aate hai
+
+//console.log(Tinder);
+
+const regularUser={
+    fullname:{
+        username:{
+            firstname:"Keshav",
+            Lastname:"Deo"
+        }
+
+    }
+}
