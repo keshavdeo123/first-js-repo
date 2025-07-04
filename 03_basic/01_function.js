ee4743f1a675cf8491d3773e0db470c0f4b184a7
Keshav Deo <keshavmaharaj345@gmail.com>
2025-07-04 03:45:13 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,16 @@
+function myName(){
+    console.log("keshav");
+    console.log("deo");
+    
+}
+//myName()
+
+function addTwoNum(number1,number2) {
+        let  sum= number1+number2
+            return sum
+                
+                    
+                    }
+                    const sum=addTwoNum(5+1,50)
+                    console.log(sum);
+
