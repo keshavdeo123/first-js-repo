1298efcc6abeeeb47a84dd6f7ce7837f40a49c79
Keshav Deo <keshavmaharaj345@gmail.com>
2025-07-09 15:40:44 +0000

    part 1 khatam

---
+++ 05_iterator/03_forEach.js
@@ -0,0 +1,26 @@
+const lang=['py','c++','java','js']
+
+lang.forEach(function(ans){
+    //console.log(ans);
+    
+})
+
+const myCoding=[
+    {
+        languageName:'js',
+        languagefile:"java"
+    },
+    {
+        languageName:'cpp',
+        languagefile:'c++'
+
+    },
+    {
+        languageName:'ruby',
+        languagefile:'rubii'
+    }
+]
+myCoding.forEach( (key)=>{
+    console.log(key.languageName);
+    
+} )
---
+++ 05_iterator/04_filter.js
@@ -0,0 +1,9 @@
+const number=[1,2,3,4,5,6,7,8,9,10]
+// const ans=number.filter( (num)=> num>4 )
+// console.log(ans);
+
+const ans=number.filter( (num)=>{
+    return num>4
+} )
+console.log(ans);
+
